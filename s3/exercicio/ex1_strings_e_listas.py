7024faf5532d1864fd118d88bfab6e41bf997196
aberraxao <diana_milu@live.com.pt>
2024-02-28 02:00:58 +0000

    Added missing ex2 and refactor

--- s3/exercicio/ex1_strings_e_listas.py
+++ s3/exercicio/ex1_strings_e_listas.py
@@ -18,34 +18,34 @@ poema = (
 
 
 def _print(alinea: str) -> None:
-    print(f"{10 * '-'} {alinea} {10 * '-'}")
+    print(f'{10 * '-'} {alinea} {10 * '-'}')
 
 
 def ex1():
-    _print("1")
-    _print("a")
+    _print('1')
+    _print('a')
     lista_versos = poema.split(' / ')
     [print(verso) for verso in lista_versos]
 
-    _print("b")
+    _print('b')
     lista_versos += [
         'Por isso eu fiz um samba bem pra frente',
         'Dizendo realmente o que é que eu acho',
         'Isso me deixa triste e cabisbaixo'
     ]
 
-    _print("c")
+    _print('c')
     print(lista_versos[-2])
     print(lista_versos[-1])
 
-    _print("d")
+    _print('d')
     numero_versos_com_samba = 0
     for verso in lista_versos:
         if 'samba' in verso:
             numero_versos_com_samba += 1
     print('numero de versos com a palavra "samba":', numero_versos_com_samba)
 
-    _print("e")
+    _print('e')
     for vogal in ['a', 'e', 'i', 'o', 'u']:
         conta = 0
         for verso in lista_versos:
@@ -55,5 +55,5 @@ def ex1():
         print(vogal, ':', conta)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     ex1()
--- s3/exercicio/ex2_dicionarios.py
+++ s3/exercicio/ex2_dicionarios.py
@@ -1,30 +1,67 @@
+from typing import Dict, List
+
+
 def _print(alinea: str) -> None:
-    print(f"{10 * '-'} {alinea} {10 * '-'}")
+    print(f'{10 * '-'} {alinea} {10 * '-'}')
 
 
 def ex2():
-    _print("2")
-    _print("a")
+    _print('2')
+    _print('a')
 
-    disciplinas_3ano_1semestre = {"AA": 19.89, "AR": 19.5, "PCD": 19.7, "PI": 19.55}
-    disciplinas_3ano_2semestre = {"DIAM": 20, "ES": 19.7, "IPM": 19.67, "PISID": 19.9}
+    disciplinas_3ano_1semestre = {'AA': 19.89, 'AR': 19.5, 'PCD': 19.7, 'PI': 19.55}
+    disciplinas_3ano_2semestre = {'DIAM': 20, 'ES': 19.7, 'IPM': 19.67, 'PISID': 19.9}
 
     print('disciplinas do semestre passado:', disciplinas_3ano_1semestre)
     print('disciplinas deste semestre:', disciplinas_3ano_2semestre)
 
-    _print("b")
+    _print('b')
     disciplinas_3ano = disciplinas_3ano_1semestre | disciplinas_3ano_2semestre
     print('disciplinas do ano letivo:', disciplinas_3ano)
 
-    _print("c")
-    print("chaves:", list(disciplinas_3ano.keys()))
+    _print('c')
+    print('chaves:', list(disciplinas_3ano.keys()))
 
-    _print("d")
-    print("valores:", list(disciplinas_3ano.values()))
+    _print('d')
+    print('valores:', list(disciplinas_3ano.values()))
 
-    _print("e")  # TODO: dúvida: é necessário mostrar as notas?
-    print("ordenado:", sorted(disciplinas_3ano.keys()))
+    _print('e')  # TODO: dúvida: é necessário mostrar as notas?
+    print('ordenado:', sorted(disciplinas_3ano.keys()))
+
+    _print('f')
+    existe_disciplina(disciplinas_3ano, 'DIAM')
+    existe_disciplina(disciplinas_3ano, 'FRC')
+
+    _print('g')
+    print('notas acima de 19:', disciplinas_com_boas_notas(disciplinas_3ano, 19))
+    print('notas acima de 19.7:', disciplinas_com_boas_notas(disciplinas_3ano, 19.7))
+
+    _print('h')
+    print('media das notas', media_das_notas(disciplinas_3ano))
+
+    _print('i')
+    print('tres melhores disciplinas', tres_melhores_disciplinas(disciplinas_3ano))
 
 
-if __name__ == "__main__":
+# TODO: posso usar Dict e List do typing module?
+def existe_disciplina(disciplinas: Dict[str, float], nome: str) -> None:
+    if nome in disciplinas.keys():
+        print(f'{nome} existe no dicionario')
+    else:
+        print(f'{nome} nao existe no dicionario')
+
+
+def disciplinas_com_boas_notas(disciplinas: Dict[str, float], nota_minima: float) -> List[str]:
+    return [disciplina for disciplina, nota in disciplinas.items() if nota > nota_minima]
+
+
+def media_das_notas(disciplinas: Dict[str, float]) -> float:
+    return sum(disciplinas.values())/len(disciplinas)
+
+
+def tres_melhores_disciplinas(disciplinas: Dict[str, float]) -> List[str]:
+    return sorted(disciplinas, key=disciplinas.get)[-3:]
+
+
+if __name__ == '__main__':
     ex2()
