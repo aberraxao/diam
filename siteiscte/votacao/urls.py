633216863d44fd7afe5a22d28b69b0df14d94559
aberraxao <diana_milu@live.com.pt>
2024-03-06 01:30:06 +0000

    django parte 2

--- siteiscte/votacao/urls.py
+++ siteiscte/votacao/urls.py
@@ -4,4 +4,7 @@ from . import views
 # o '.' significa que importa views da mesma directoria)
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # ex: votacao/3
+    path('<int:questao_id>', views.detalhe, name='detalhe'),
 ]
--- siteiscte/votacao/views.py
+++ siteiscte/votacao/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from votacao.models import Questao, Opcao
 
 
 def index(request):
     return HttpResponse("<h1>Esta é a página de entrada da app votacao</h1>")
+
+
+def detalhe(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    return render(request, 'votacao/detalhe.html', {'questao': questao})
+
+
+def voto(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    try:
+        opcao_seleccionada = questao.opcao_set.get(pk=request.POST['opcao'])
+    except (KeyError, Opcao.DoesNotExist):
+        return render(request, 'votacao/detalhe.html', {'questao': questao, 'error_message': "Não escolheu uma opção"})
+    else:
+        opcao_seleccionada.votos += 1
+        opcao_seleccionada.save()
+        return HttpResponseRedirect(reverse('votacao:resultados', args=(questao.id,)))
