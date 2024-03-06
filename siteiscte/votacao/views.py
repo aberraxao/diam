259b9336b966193a2d7c28adb191c96ade11e218
aberraxao <diana_milu@live.com.pt>
2024-03-06 02:35:14 +0000

    django part 2 end

--- siteiscte/votacao/views.py
+++ siteiscte/votacao/views.py
@@ -6,7 +6,9 @@ from votacao.models import Questao, Opcao
 
 
 def index(request):
-    return HttpResponse("<h1>Esta é a página de entrada da app votacao</h1>")
+    latest_question_list = Questao.objects.order_by('-pub_data')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'votacao/index.html', context)
 
 
 def detalhe(request, questao_id):
@@ -14,13 +16,26 @@ def detalhe(request, questao_id):
     return render(request, 'votacao/detalhe.html', {'questao': questao})
 
 
+def resultados(request, questao_id):
+    questao = get_object_or_404(Questao, pk=questao_id)
+    return render(request, 'votacao/resultados.html', {'questao': questao})
+
+
 def voto(request, questao_id):
     questao = get_object_or_404(Questao, pk=questao_id)
     try:
         opcao_seleccionada = questao.opcao_set.get(pk=request.POST['opcao'])
+
     except (KeyError, Opcao.DoesNotExist):
-        return render(request, 'votacao/detalhe.html', {'questao': questao, 'error_message': "Não escolheu uma opção"})
+        # Apresenta de novo o form para votar
+        return render(request, 'votacao/detalhe.html',
+                      {'questao': questao, 'error_message': "Não escolheu uma opção", })
     else:
         opcao_seleccionada.votos += 1
         opcao_seleccionada.save()
-        return HttpResponseRedirect(reverse('votacao:resultados', args=(questao.id,)))
+        # Retorne sempre HttpResponseRedirect depois de
+        # tratar os dados POST de um form
+        # pois isso impede os dados de serem tratados
+        # repetidamente se o utilizador
+        # voltar para a página web anterior.
+    return HttpResponseRedirect(reverse('votacao:resultados', args=(questao.id,)))
